28e3ea8a3d91811a9fba49d827f39447a533fc0f
Ben Waldman <bmwaldman0918@gmail.com>
2025-03-02 16:30:32 -0600

    organization

--- Pro4.lean
+++ Pro4.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Pro4` library.
 -- Import modules here that should be built as part of the library.
-import «Pro4».Basic
+import «Pro4».Correctness
+import «Pro4».Sieve
+import «Pro4».Primes
