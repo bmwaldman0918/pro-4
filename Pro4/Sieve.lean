bfe479947954ef5f129f65e39a5e7afb5eb8bdff
Kelly <kellymao@uchicago.edu>
2025-03-02 17:36:28 -0600

    in the process of changing sieve.lean

--- Pro4/Sieve.lean
+++ Pro4/Sieve.lean
@@ -1,22 +1,23 @@
+import Mathlib.Data.Stream.Defs
 
-def setDiff (l l' : Stream Nat Nat) : Stream Nat Nat :=
-  let x := stream.head l
-  let xs := stream.tail l
+partial def setDiff (l l' : Stream' Nat) : Stream' Nat :=
+  let x := Stream'.head l
+  let xs := Stream'.tail l
 
-  let y := stream.head l'
-  let ys := stream.tail l'
+  let y := Stream'.head l'
+  let ys := Stream'.tail l'
 
   if x < y
-    then stream.cons x (setDiff xs l')
+    then Stream'.cons x (setDiff xs l')
   else if x == y
     then setDiff xs ys
   else -- if x > y
     setDiff l ys
 
-def makeP (f : Nat) (l : Stream Nat Nat) : Stream Nat Nat :=
-  2 :: (setDiff (stream f) l)
+def makeP (f : Nat) (l : Stream' Nat) : Stream' Nat :=
+  2 :: (setDiff (Stream' Nat) l)
 
-def makeC (l : List Nat) : List Nat :=
+-- def makeC (l : List Nat) : List Nat :=
 
 def eratosthenes (fuel : Nat) : List Nat :=
   match fuel with
