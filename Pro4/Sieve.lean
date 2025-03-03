c5d0c6a45fc1d8a7221ff71d7b068538b031e5f8
Kelly <kellymao@uchicago.edu>
2025-03-02 18:05:04 -0600

    working on fixing up makeP and makeC

--- Pro4/Sieve.lean
+++ Pro4/Sieve.lean
@@ -1,5 +1,7 @@
 import Mathlib.Data.Stream.Defs
 
+-- set difference
+-- elements in l but NOT in l'
 partial def setDiff (l l' : Stream' Nat) : Stream' Nat :=
   let x := Stream'.head l
   let xs := Stream'.tail l
@@ -14,8 +16,13 @@ partial def setDiff (l l' : Stream' Nat) : Stream' Nat :=
   else -- if x > y
     setDiff l ys
 
-def makeP (f : Nat) (l : Stream' Nat) : Stream' Nat :=
-  2 :: (setDiff (Stream' Nat) l)
+-- natural numbers starting from 3
+def natsThree : Stream' Nat :=
+  Stream'.drop 3 Stream'.nats
+
+-- L is the list of composites
+def makeP (l : Stream' Nat) : Stream' Nat :=
+  Stream'.cons 2 (setDiff (natsThree) l)
 
 -- def makeC (l : List Nat) : List Nat :=
 
