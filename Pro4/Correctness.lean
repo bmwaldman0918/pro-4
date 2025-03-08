2c11c642b18ba2d80ebcee80acfff686c6d18840
Ben Waldman <bmwaldman0918@gmail.com>
2025-03-08 16:04:22 -0600

    proof statement

--- Pro4/Correctness.lean
+++ Pro4/Correctness.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Stream.Defs
+import Pro4.Sieve
 
 -- proving the correctness of the sieve
 -- take lemma -- infinite lists are equal if every shared prefix is equal
@@ -6,5 +7,7 @@ import Mathlib.Data.Stream.Defs
 -- then that those arbitrary primes are exactly the correct first x primes
 
 -- we need a canonical stream of primes to compare with
+def primes' (f : fuel) : Stream' Nat := sorry
 
--- theorem sieve_correct : ∀ n, ∃ f, (Stream'.take primes n) =
+theorem sieve_correct : ∀ (n m : Nat), ∃ f, (Stream'.take n (primes f)) = (Stream'.take n (primes' m)) :=
+  by sorry
