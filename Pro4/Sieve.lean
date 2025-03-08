de6c3d75bf07b4830881855a453984c0de30f755
Kelly <kellymao@uchicago.edu>
2025-03-07 22:16:05 -0600

    tentative completion of all fns

--- Pro4/Sieve.lean
+++ Pro4/Sieve.lean
@@ -2,7 +2,7 @@ import Mathlib.Data.Stream.Defs
 
 -- FUEL: infinite generator
 -- L, L': infinite lists
--- All elements in L but not in L'.
+-- Set difference; or, all elements in L but not in L'.
 def setDiff (fuel : Nat) (l l' : Stream' Nat) : Stream' Nat :=
   match fuel with
   | Nat.zero => Stream'.const 0
@@ -24,11 +24,6 @@ def setDiff (fuel : Nat) (l l' : Stream' Nat) : Stream' Nat :=
 def natsThree : Stream' Nat :=
   Stream'.drop 3 Stream'.nats
 
--- FUEL: infinite generator
--- L: infinite list of composites
-def makeP (fuel: Nat) (l : Stream' Nat) : Stream' Nat :=
-  Stream'.cons 2 (setDiff fuel natsThree l)
-
 -- All multiples of P starting with its square.
 def multiples (fuel: Nat) (p: Nat): List Nat :=
   match fuel with
@@ -36,9 +31,48 @@ def multiples (fuel: Nat) (p: Nat): List Nat :=
   | Nat.succ m =>
     (multiples m p) ++ [p * (p + m)]
 
--- def mergeAll
+-- Set union of two lists; or, merging two
+-- strictly increasing streams L and L' into one.
+def merge (l l' : List Nat) : List Nat :=
+  match l, l' with
+  | [], _ => l'
+  | _, [] => l
+  | x :: xs, y :: ys =>
+    if x < y
+      then x :: (merge xs (y :: ys))
+    else if x == y
+      then x :: (merge xs ys)
+    else -- x > y
+      y :: (merge (x :: xs) ys)
 
--- def makeC (l : List Nat) : List Nat :=
+-- Helper function for mergeAll, which assumes
+-- a sorted stream of sorted streams.
+def xmerge (l l' : List Nat) : List Nat :=
+  match l with
+  | [] => l'
+  | x :: xs => x :: (merge xs l')
+
+-- Merges a sorted stream of sorted streams.
+def mergeAll (l : List (List Nat)) : List Nat :=
+  match l with
+  | [] => []
+  | xs :: xss => xmerge xs (mergeAll xss)
+
+-- FUEL: infinite generator
+-- L: infinite list of composites
+-- Set difference between a list of definite composite numbers
+-- and list of natural numbers (known primes up to FUEL).
+def makeP (fuel: Nat) (l : Stream' Nat) : Stream' Nat :=
+  Stream'.cons 2 (setDiff fuel natsThree l)
+
+-- FUEL: infinite generator
+-- L: infinite list of primes
+-- Create a list of all multiples of the first FUEL
+-- primes (known composites).
+def makeC (fuel: Nat) (l : Stream' Nat) : Stream' Nat :=
+  Stream'.appendStream'
+    (mergeAll (List.map (multiples fuel) (Stream'.take fuel l)))
+    (Stream'.const 0)
 
 mutual
   def primes (fuel : Nat) : Stream' Nat :=
@@ -50,3 +84,5 @@ mutual
       match fuel with
     | Nat.zero => Stream'.const 0
     | Nat.succ f => makeC f (primes f)
+
+end
